f51650fbb2897891db92d483785600b963606802
Zack Havens <Zack.Havens@arm.com>
2016-09-07 15:46:25 -0500

    Initial version

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'LPC1857 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LPC18xx.h"
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_DEVICE_STARTUP_LPC18XX      /* Device Startup for NXP18XX */
+
+#endif /* RTE_COMPONENTS_H */
